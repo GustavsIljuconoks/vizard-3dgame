4b63b98755c15ab02d490098a02776109cfcbdb2
Gustavs Iļjučonoks <gustavs.iljuconoks@cube.lv>
2024-06-11 17:04:29 +0300

    finalize element picking

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 ﻿import viz
 import vizact
-import vizcam
 
 # Set basic settings
-viz.go() # viz.FULLSCREEN
+viz.go() #viz.FULLSCREEN
 viz.MainView.setPosition(0, 5, 2)
 viz.MainView.setEuler(0, 135, 180)
 viz.setMultiSample(4)
@@ -11,14 +10,10 @@ viz.fov(60)
 viz.mouse(viz.OFF)
 
 gameScene = viz.add('resources/chessgame.gltf')
-# WASD movment
-# tracker = vizcam.addWalkNavigate(moveScale=10.0)
-# tracker.setPosition([0, 2, 1.5])
-# viz.link(tracker,viz.MainView)
-# viz.mouse.setVisible(False)
 
-soccerball = viz.addChild('soccerball.osgb')
-soccerball.setPosition([-0.5, 4, -1])
+# TODO: Create element spawner function
+bishop = viz.addChild('resources/bishop.obj')
+bishop.setPosition([0, 0, -0.05])
 
 picked_object = None
 def onPick():
@@ -30,7 +25,8 @@ def onPick():
 def mymouse(e):
     global picked_object
     if picked_object:
-        picked_object.setPosition([(e.x - 0.5) * 6, e.y * 4, 5])
+        _, current_y, _ = picked_object.getPosition()
+        picked_object.setPosition([(e.x - 0.5) * 6, current_y, e.y * 4])
 
 vizact.onmousedown(viz.MOUSEBUTTON_LEFT, onPick)
 viz.callback(viz.MOUSE_MOVE_EVENT, mymouse)
